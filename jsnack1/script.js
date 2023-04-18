262d6a0816139f38b419cbbf16840b71b39acdd1
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-04-18 16:41:05 +0200

    add for cycle and conditions

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,21 @@
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
+
+// const inputNum = document.querySelector('input');
+const arrNum = [];
+console.log(arrNum);
+
+
+for (let i = 0; i < 6; i++){
+    
+    let numUser = parseInt(prompt('scrivi qui un numero intero'));
+    console.log('numUser:', numUser);
+
+    if (numUser %2 != 0){
+        arrNum.push(numUser);
+        console.log(arrNum);
+    }else if (numUser == NaN || numUser %2 == 0){
+        console.log(arrNum);
+    }
+}
