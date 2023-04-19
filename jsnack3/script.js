a484750f209f139893b0de1187f7f4a5e93498b5
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-04-19 10:49:56 +0200

    add for cycle and if

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,14 @@
 // 3) snack 2.3
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const arrNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for (let i = 0; i < arrNums.length; i++){
+    if (i % 2 == 1){
+        sum += arrNums[i];
+        console.log(sum);
+    }
+}
+
+console.log('risultato:', sum);
